b299a4cda073d930c723338a7f5c4e7e8db2e8d3
Shukurov Shahrom <112476380+MOR8T@users.noreply.github.com>
2022-10-25 15:44:41 +0500

    commit4

--- README.md
+++ README.md
@@ -5,7 +5,13 @@
 ## Synchronous
 ![](./img/js1.jpg)
 ## Asynchronous
+![](./img/js3.jpg)
+## method setTimeout()
 ![](./img/js2.jpg)
+## method setTimeout()
+![](./img/js4.jpg)
+#
 ## #2 CallBack and Promise
 ![]()
+#
 ## #3 Try/catch/finally and Asunc await
--- main.js
+++ main.js
@@ -5,19 +5,35 @@
 // console.log(3);
 // console.log(4);
 
-setTimeout(()=>
-    console.log(0)
-,0);
+// setTimeout(()=>
+// console.log(1)
+// ,1000);
+// console.log(2);
+// console.log(3);
 
-setTimeout(()=>
-    console.log(1)
-,1000);
+// setTimeout(()=>
+//     console.log(0)
+// ,0);
 
-console.log(2);
+// setTimeout(()=>
+//     console.log(1)
+// ,1000);
 
-setTimeout(()=>
-    console.log(3)
-,2000);
+// console.log(2);
 
-console.log(4);
+// setTimeout(()=>
+//     console.log(3)
+// ,2000);
+
+// console.log(4);
+
+
+// let cnt = 0;
+
+// setInterval(()=>{
+// cnt++;
+// let h1 = document.createElement("h1");
+// document.body.appendChild(h1);
+// h1.innerHTML = cnt;
+// },2000);
 
