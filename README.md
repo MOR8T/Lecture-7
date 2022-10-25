e7d85f649ba6c8ac1d613d15f76bb9917ec78f78
Shukurov Shahrom <112476380+MOR8T@users.noreply.github.com>
2022-10-25 15:20:41 +0500

    commit3

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 ## #1 Synchronous and Asynchronous
 ![](./img/fon.jpg)
 ## Synchronous
-![]()
+![](./img/js1.jpg)
 ## Asynchronous
-![]()
+![](./img/js2.jpg)
 ## #2 CallBack and Promise
 ![]()
 ## #3 Try/catch/finally and Asunc await
--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 // #1
 
-console.log(1);
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+
+setTimeout(()=>
+    console.log(0)
+,0);
+
+setTimeout(()=>
+    console.log(1)
+,1000);
+
 console.log(2);
-console.log(3);
+
+setTimeout(()=>
+    console.log(3)
+,2000);
+
 console.log(4);
+
